e2ac7dc9d42c9cbd4c7e1287760821b0d13be105
Ryan Broadsword <rbroadsword@gmail.com>
2022-09-09 22:06:57 -0700

    add migrations for SLQ

---
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Models
+{
+    public class AnimalShelterContext : DbContext
+    {
+        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Dog> Dogs { get; set; }
+        public DbSet<Cat> Cats { get; set; }
+    }
+}
