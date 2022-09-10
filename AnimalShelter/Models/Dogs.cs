11c242ddaa04f7d97b013ecd085a9082c50f86d0
Ryan Broadsword <rbroadsword@gmail.com>
2022-09-09 21:44:53 -0700

    create models for cats/dogs

---
+++ AnimalShelter/Models/Dogs.cs
@@ -0,0 +1,10 @@
+namespace AnimalShelter.Models
+{
+  public class Dog
+  {
+    public int DogId { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Bio { get; set; }
+  }
+}
